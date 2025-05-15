045c4364ed5aa096d39a53f19984940d42ced7ee
feyzancolak <62944174+feyzancolak@users.noreply.github.com>
2025-05-15 13:29:01 +0200

    first trial of main

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,23 @@
 #include "Timer.hpp"
 
 int main() {
-  // Print a simple message
-    std::cout << "Hello, this is the Sudoku Validator!" << std::endl;
+    int puzzle[9][9] = {
+        {5, 3, 0, 0, 7, 0, 0, 0, 0},
+        {6, 0, 0, 1, 9, 5, 0, 0, 0},
+        {0, 9, 8, 0, 0, 0, 0, 6, 0},
+        {8, 0, 0, 0, 6, 0, 0, 0, 3},
+        {4, 0, 0, 8, 0, 3, 0, 0, 1},
+        {7, 0, 0, 0, 2, 0, 0, 0, 6},
+        {0, 6, 0, 0, 0, 0, 2, 8, 0},
+        {0, 0, 0, 4, 1, 9, 0, 0, 5},
+        {0, 0, 0, 0, 8, 0, 0, 7, 9}
+    };
+
+    std::cout << "Solving with sequential solver...\n";
+    solveSudokuSequential(puzzle);
+
+    std::cout << "Solving with naive parallel solver...\n";
+    solveSudokuParallelNaive(puzzle);
+
+    return 0;
 }
